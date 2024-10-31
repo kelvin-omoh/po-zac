be9e67a73c5e08da62e35f173a0046ea326ef546
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2024-10-31 13:26:46 +0100

    updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "POSACRO",
-  description: "POSACRO - Positive Acronym",
+  title: "PO-ZAC",
+  description: "PO-ZAC Game  also known as mind shield and bullying-proof game has been developed to train the mind to respond positively to any form of verbal bullying. It is intended to ultimately develop children and adolescents to be emotionally resilient and mentally strong and prepared against verbal bullying. This game is a form of bibliotherapy intervention to mitigate the negative effect of verbal bullying—specifically, the use of insulting and abusive words. PO-ZAC serves as mind shield to resist verbal bullying and proof against feelings that could cause trauma and alienation socially and emotionally.",
 };
 
 export default function RootLayout({
