d3db82cc315427e16c63014dabf340b92b677f9b
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2024-11-07 15:55:14 +0100

    updated

--- src/app/components/Modal/Modal.jsx
+++ src/app/components/Modal/Modal.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Modal = (props) => {
     return (
-        <div className={`w-screen absolute h-screen z-[500] top-0 left-0 flex justify-center items-center 
+        <div className={`w-screen absolute min-h-[100vh] z-[500] top-0 left-0 flex justify-center items-center 
         ${props.blur ? "bg-[rgba(0, 0, 0, 0.4)] backdrop-blur-[15px]" : "bg-blue-950 backdrop-blur-[15px]"} cursor-pointer`}>
-            <div id="modal-content" className='bg-[url("/game/wood-bg.jpg")] w-full p-4 rounded-2xl mx-4 sm:mx-0 sm:p-8 cursor-default'>
+            <div id="modal-content" className='bg-[url("/game/wood-bg.jpg")] md:h-[100%] md:w-[40%] w-full p-4 rounded-2xl mx-4 sm:mx-0 sm:p-8 cursor-default'>
                 {props.children}
             </div>
         </div>
